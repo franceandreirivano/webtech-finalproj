21a4e8ea9174647fa2e0812fc2e9b83d068ba888
franceandreirivano <franceandreirivano@gmail.com>
2026-06-17 01:49:42 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Inventory Management System</title>
+</head>
+<body>
+    <h1>Inventory Management System</h1>
+
+    <ul>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="viewinventory.php">View Inventory</a></li>
+        <li><a href="addinventory.php">Add Inventory</a></li>
+    </ul>
+</body>
+</html>
